884589fcdaedef5419756ab551977df46d2b97e5
kumaboie <sae19960111@gmail.com>
2024-12-28 18:41:02 +0900

    新規登録/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must contain a valid '@' symbol" }
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :furigana_first_name, presence: true
-  validates :furigana_last_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :furigana_first_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :furigana_last_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birth_date, presence: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
 end
