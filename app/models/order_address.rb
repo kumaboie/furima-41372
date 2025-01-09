b9f386a842e248f06305c1f42528d3396941b23f
kumaboie <sae19960111@gmail.com>
2025-01-09 21:59:24 +0900

    バリデーションの訂正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,10 +3,10 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :city
     validates :addresses
-    validates :phone_number, length: { maximum: 11, minimum: 10 }, format: { with: /\A[0-9]+\z/ }
+    validates :phone_number, format: { with: /\A[0-9]+\z/ }
     validates :token
     validates :user_id
     validates :item_id
