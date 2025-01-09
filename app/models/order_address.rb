6f9afd0e19349841cc6c327dc9819e254f62c281
kumaboie <sae19960111@gmail.com>
2025-01-09 20:08:39 +0900

    モデルにバレデーションを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,6 +8,8 @@ class OrderAddress
     validates :addresses
     validates :phone_number, length: { maximum: 11, minimum: 10 }, format: { with: /\A[0-9]+\z/ }
     validates :token
+    validates :user_id
+    validates :item_id
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
