d717d76e6e5726ee5f1a433b7b3303ab645c36aa
kumaboie <sae19960111@gmail.com>
2025-01-09 20:14:36 +0900

    FactoryBot.buildの引数を修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OrderAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
-    @order_address = FactoryBot.build(:order_address)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
 
   describe 'アイテムの購入' do
